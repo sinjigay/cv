7bf9ca92a9218da3844c2502d62f224047b0b458
Yahor Apimakhovich <145188201+sinjigay@users.noreply.github.com>
2024-12-22 11:48:28 +0300

    Update LinkList.jsx

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,13 +1,13 @@
 export const LinkList = ({ title, items }) => {
   return (
     <div>
-      <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+      <h2 className="mb-2 text-lg font-semibold text-gray-900">
         {title}
       </h2>
       <ul>
         {items.map((it) => (
           <li
-            className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400"
+            className="max-w-md space-y-1 text-gray-500 list-disc list-inside"
             key={it}
           >
             <a href={it.link}>{it.title}</a>
