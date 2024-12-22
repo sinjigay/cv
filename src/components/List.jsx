dbf3193532b8d584a9bd39f0b8a9998ebec1a1e8
Yahor Apimakhovich <145188201+sinjigay@users.noreply.github.com>
2024-12-22 11:48:56 +0300

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,10 @@
 export const List = ({title, items}) => {
     return (
         <div>
-            <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">{title}</h2>
+            <h2 className="mb-2 text-lg font-semibold text-gray-900">{title}</h2>
             <ul>
             {items.map(it => (
-                <li className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400" key={it}>{it}</li>
+                <li className="max-w-md space-y-1 text-gray-500 list-disc list-inside" key={it}>{it}</li>
             ))}
             </ul>
         </div>
